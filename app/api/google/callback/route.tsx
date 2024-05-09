2208226e428b7cf0dccebf046ffe42a37b0f5533
MetriSomesh <someshmetri44@gmail.com>
2024-05-09 16:14:49 +0530

    OAuth token is been created

--- app/api/google/callback/route.tsx
+++ app/api/google/callback/route.tsx
@@ -1,64 +1,39 @@
-// import { google } from "googleapis";
-// import { NextRequest, NextResponse } from "next/server";
-// import OAUTH2Data from "../../../../credentials.json";
+import { google } from "googleapis";
+import { NextRequest, NextResponse } from "next/server";
+import OAUTH2Data from "../../../../credentials.json";
 
-// const CLIENT_ID = OAUTH2Data.web.client_id;
-// const CLIENT_SECRET = OAUTH2Data.web.client_secret;
-// const REDIRECT_URL = OAUTH2Data.web.redirect_uris[0];
+export const GET = async (req: NextRequest) => {
+  const CLIENT_ID = OAUTH2Data.web.client_id;
+  const CLIENT_SECRET = OAUTH2Data.web.client_secret;
+  const REDIRECT_URL = OAUTH2Data.web.redirect_uris[0];
 
-// const oAuth2Client = new google.auth.OAuth2(
-//   CLIENT_ID,
-//   CLIENT_SECRET,
-//   REDIRECT_URL
-// );
+  const oAuth2Client = new google.auth.OAuth2(
+    CLIENT_ID,
+    CLIENT_SECRET,
+    REDIRECT_URL
+  );
 
-// // export default function GoogleAuth({NextResponse})  {
-// //   const scopes = [
-// //     "https://www.googleapis.com/auth/youtube.upload https://www.googleapis.com/auth/userinfo.profile",
-// //   ];
-// //   return oAuth2Client.generateAuthUrl({
-// //     access_type: "offline",
-// //     scope: scopes,
-// //   });
-// // };
+  // const { code } = await req.nextUrl.searchParams.get('code') || "" // Assuming the authorization code is passed as a query parameter
+  const queryParams = new URLSearchParams(req.nextUrl.searchParams);
+  const code = queryParams.get("code");
 
-// export const GET = async (req: NextRequest) => {
-//   const CLIENT_ID = OAUTH2Data.web.client_id;
-//   const CLIENT_SECRET = OAUTH2Data.web.client_secret;
-//   const REDIRECT_URL = OAUTH2Data.web.redirect_uris[0];
-
-//   const oAuth2Client = new google.auth.OAuth2(
-//     CLIENT_ID,
-//     CLIENT_SECRET,
-//     REDIRECT_URL
-//   );
-//   const scopes = [
-//     "https://www.googleapis.com/auth/youtube.upload https://www.googleapis.com/auth/userinfo.profile",
-//   ];
-//   try {
-//     const authUrl = oAuth2Client.generateAuthUrl({
-//       access_type: "offline",
-//       scope: scopes,
-//     });
-
-//     // return NextResponse.json(
-//     //   {
-//     //     msg: "Authentication SuccessFully",
-//     //   },
-//     //   { status: 200 }
-//     // );
-//     return NextResponse.json(authUrl);
-
-//   } catch (error) {
-//     return NextResponse.json(
-//       {
-//         msg: "Something went wrong",
-//       },
-//       { status: 500 }
-//     );
-//   }
-// };
-
-export default function GoogleAuth() {
-  return <div>hello</div>;
-}
+  try {
+    // Exchange authorization code for access token
+    const { tokens } = await oAuth2Client.getToken(code as string);
+    const accessToken = tokens.access_token;
+    // const expiryTime = new Date().getTime() + tokens.expires_in * 1000;
+    // Return the access token
+    return NextResponse.json({ accessToken });
+  } catch (error) {
+    console.error(
+      "Error exchanging authorization code for access token:",
+      error
+    );
+    return NextResponse.json(
+      {
+        msg: "Failed to obtain access token",
+      },
+      { status: 500 }
+    );
+  }
+};

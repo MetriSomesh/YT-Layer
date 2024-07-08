8a2c3dc9c2d27a6c44086165ea64b4877a2b9005
MetriSomesh <someshmetri44@gmail.com>
2024-07-08 09:10:54 +0530

    updated channel,editor cards

--- components/ChannelCard.tsx
+++ components/ChannelCard.tsx
@@ -1,28 +1,36 @@
 import { channelLinkState } from "@/app/state/channelLinkState";
 import { useRecoilState } from "recoil";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 export const ChannelCard = () => {
   const [channelLink, setChannelLink] = useRecoilState(channelLinkState);
+
   return (
-    <div className="max-w-sm p-6 border rounded-lg shadow bg-gray-800 border-gray-700 text-center h-56">
-      <a href="#">
-        <h5 className="mb-2 text-2xl font-bold tracking-tight text-white">
-          View Youtube Channel
-        </h5>
-      </a>
-      <p className="mb-3 font-normal text-gray-400">
-        Take a look at your Youtube channel statistics from here
-      </p>
-      <div className="text-center mt-5 mb-5">
-        <a
-          href={`https://youtube.com/channel/${channelLink}`}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="inline-flex items-center px-3 py-2 text-sm font-medium text-white rounded-lg focus:ring-4 focus:outline-none bg-blue-600 hover:bg-blue-700 focus:ring-blue-800"
+    <Card className="w-[400px] text-center">
+      <CardHeader className="mt-2">
+        <CardTitle className="justify-center mb-2">View Channel</CardTitle>
+        <CardDescription className="mt-2">
+          Take a look at your Youtube Channel and check statistics from here
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="flex justify-center">
+        <Button
+          className="mt-2 w-[90px]"
+          onClick={() => {
+            window.open(`https://youtube.com/channel/${channelLink}`, "_blank");
+          }}
         >
-          View Channel
-        </a>
-      </div>
-    </div>
+          View
+        </Button>
+      </CardContent>
+    </Card>
   );
 };
--- components/EditorCard.tsx
+++ components/EditorCard.tsx
@@ -1,22 +1,33 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 export const EditorCard = () => {
   return (
-    <div className="max-w-sm p-6  border  rounded-lg shadow bg-gray-800 border-gray-700 text-center  h-56">
-      <a href="#">
-        <h5 className="mb-2 text-2xl font-bold tracking-tight  text-white ">
-          Add Editor
-        </h5>
-      </a>
-      <p className="mb-3 font-normal  text-gray-400">
-        Select editor of your choice for your youtube channel.
-      </p>
-      <div className="text-center mt-5 mb-5">
-        <a
-          href="/addeditor"
-          className="inline-flex items-center px-3 py-2 text-sm font-medium  text-white  rounded-lg  focus:ring-4 focus:outline-none  bg-blue-600 hover:bg-blue-700 focus:ring-blue-800"
+    <Card className="w-[400px] text-center">
+      <CardHeader className="mt-2">
+        <CardTitle className="justify-center mb-2">Add Editor</CardTitle>
+        <CardDescription className="mt-2">
+          Select editor of your choice for your youtube channel. Send invitation
+          to an editor.
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="flex justify-center">
+        <Button
+          className="mt-2 w-[90px]"
+          onClick={() => {
+            window.location.href = "http://localhost:3000/addeditor";
+          }}
         >
-          Add Editor
-        </a>
-      </div>
-    </div>
+          Add
+        </Button>
+      </CardContent>
+    </Card>
   );
 };
